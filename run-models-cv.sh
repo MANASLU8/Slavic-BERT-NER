e060ce411ce9c318656ea016fcf967df2f1de05e
Dmitrii <zeionara@niuitmo.ru>
2020-01-23 18:15:54 +0300

    Added script for running models

---
+++ run-models-cv.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+NUMBER_OF_FOLDS=${1:-10}
+NER_COMPARISON_HOME=/home/nami/ner-comparison
+
+. ./python/bin/activate
+LD_LIBRARY_PATH=/usr/local/cuda/lib64
+
+for model in ner_ontonotes_bert ner_ontonotes_bert_mult ner_rus_bert ner_rus ner_ontonotes ner_ontonotes slavic
+do
+	mkdir $NER_COMPARISON_HOME/cv/$model
+	for i in $(seq -f "%02g" 1 $NUMBER_OF_FOLDS)
+	do
+		python predict.py --model $model --input $NER_COMPARISON_HOME/cv/reference/$i.txt --output $NER_COMPARISON_HOME/cv/$model/$i.txt
+	done
+done
+
+deactivate
